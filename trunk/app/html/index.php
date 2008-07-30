1837ca2a5f6b108139edd58a3894b296e30b030d
daniel.oconnor <daniel.oconnor@deb523c1-4153-0410-b00f-251cc64871bd>
2008-07-30 18:52:21 +0000

    Made untestable, global configuration.

--- trunk/app/html/index.php
+++ trunk/app/html/index.php
@@ -15,10 +15,24 @@ $db = Zend_Db::factory($config->db->type, array(
     'dbname'   => $config->db->name
 ));
 
+$store = ARC2::getStore(array(
+                        'db_name' => $config->arc->db->name,
+                        'db_user' => $config->arc->db->username,
+                        'db_pwd' =>  $config->arc->db->password,
+                        'db_host' =>  $config->arc->db->host,
+                        'store_name' => $config->arc->db->name
+                        ));
+
+
 
 Zend_Db_Table::setDefaultAdapter($db);
 
 Zend_Layout::startMvc();
+
+$front = Zend_Controller_Front::getInstance();
+$front->setParam('db', $db);
+$front->setParam('config', $config);
+$front->setParam('store', $store);
 
 Zend_Controller_Front::run(dirname(__FILE__) . '/../controllers');
 
