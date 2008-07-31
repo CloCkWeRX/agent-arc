5f577bb7565cb13faab6b33f6088345440374654
daniel.oconnor <daniel.oconnor@deb523c1-4153-0410-b00f-251cc64871bd>
2008-07-31 03:55:54 +0000

    Added the ability to trigger consolidation

--- app/modules/foafinference/controllers/IndexController.php
+++ app/modules/foafinference/controllers/IndexController.php
@@ -3,21 +3,22 @@ class FoafInference_IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
-        /*
-$my_ifp = 'http://xmlns.com/foaf/0.1/homepage';
-$store->consolidateIFP($my_ifp);
+        $config = Zend_Controller_Front::getInstance()->getParam('config');
+        $smush = $config->foafinference->smush;
 
-$my_ifps = array(
-  'http://xmlns.com/foaf/0.1/homepage',
-  'http://gmpg.org/xfn/11#me',
-  'http://www.w3.org/2002/07/owl#sameAs',
-  'http://xmlns.com/foaf/0.1/mbox_sha1sum',
-  'http://xmlns.com/foaf/0.1/openid',
-  'http://xmlns.com/foaf/0.1/weblog'
-);
-$store->setSetting('ifps', $my_ifps);
-$store->consolidate();
-        */
+        $store = Zend_Controller_Front::getInstance()->getParam('store');
+
+
+        $ifps = array('http://xmlns.com/foaf/0.1/homepage');
+        if ($smush) {
+
+            foreach ($ifps as $ifp) {
+                $store->consolidateIFP($ifp);
+            }
+        }
+
+        $this->view->ifps = $ifps;
+        $this->view->smush = $smush;
     }
 
 }
