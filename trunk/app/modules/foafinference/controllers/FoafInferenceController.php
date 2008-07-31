6302b7ef30cdf18b782efdd976453947704cc23b
daniel.oconnor <daniel.oconnor@deb523c1-4153-0410-b00f-251cc64871bd>
2008-07-31 02:27:17 +0000

    Moved

---
+++ trunk/app/modules/foafinference/controllers/FoafInferenceController.php
@@ -0,0 +1,24 @@
+<?php
+class FoafInferenceController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        /*
+$my_ifp = 'http://xmlns.com/foaf/0.1/homepage';
+$store->consolidateIFP($my_ifp);
+
+$my_ifps = array(
+  'http://xmlns.com/foaf/0.1/homepage',
+  'http://gmpg.org/xfn/11#me',
+  'http://www.w3.org/2002/07/owl#sameAs',
+  'http://xmlns.com/foaf/0.1/mbox_sha1sum',
+  'http://xmlns.com/foaf/0.1/openid',
+  'http://xmlns.com/foaf/0.1/weblog'
+);
+$store->setSetting('ifps', $my_ifps);
+$store->consolidate();
+        */
+    }
+
+}
+
